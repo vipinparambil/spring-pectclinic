5df5f589be98819c04c595539f4b25ac6c1fdd2f
Vipin <vipin.pp@xerox.nt>
2020-04-23 12:09:43 +0530

    Initial app creation

---
+++ pet-clinic-start/src/main/java/com/projectx/petclinicstart/PetClinicStartApplication.java
@@ -0,0 +1,13 @@
+package com.projectx.petclinicstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicStartApplication.class, args);
+	}
+
+}
---
+++ pet-clinic-start/src/test/java/com/projectx/petclinicstart/PetClinicStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectx.petclinicstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
