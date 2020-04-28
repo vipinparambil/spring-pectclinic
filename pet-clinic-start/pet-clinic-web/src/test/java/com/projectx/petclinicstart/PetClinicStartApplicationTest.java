c48bb00b46fce44a08b300be217a6ef381ef6e98
Vipin <vipin.pp@xerox.nt>
2020-04-28 21:31:53 +0530

    Created multi module for data and web

--- pet-clinic-start/pet-clinic-web/src/test/java/com/projectx/petclinicstart/PetClinicStartApplicationTest.java
+++ pet-clinic-start/pet-clinic-web/src/test/java/com/projectx/petclinicstart/PetClinicStartApplicationTest.java
@@ -1,13 +1,10 @@
 package com.projectx.petclinicstart;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PetClinicStartApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+@SpringBootTest
+public class PetClinicStartApplicationTest {
+
 
 }
