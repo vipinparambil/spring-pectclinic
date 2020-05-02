e207751b38898e77dbaa87371c067ad480f639ad
vipinparambil <vipinparambil@gmail.com>
2020-05-02 22:59:54 +0530

    Displayed the vet list with from the loaded map

--- pet-clinic-start/pet-clinic-web/src/main/java/com/projectx/petclinicstart/controllers/VetController.java
+++ pet-clinic-start/pet-clinic-web/src/main/java/com/projectx/petclinicstart/controllers/VetController.java
@@ -1,6 +1,8 @@
 package com.projectx.petclinicstart.controllers;
 
+import com.projectx.petclinicstart.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"", "/index"})
-    public  String listVets(){
+    public  String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
