aa73c300732366f5508bb179282c09cd04689351
vipinparambil <vipinparambil@gmail.com>
2020-05-02 22:55:00 +0530

    Displayed the owner list with from the loaded map

--- pet-clinic-start/pet-clinic-web/src/main/java/com/projectx/petclinicstart/controllers/OwnerController.java
+++ pet-clinic-start/pet-clinic-web/src/main/java/com/projectx/petclinicstart/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package com.projectx.petclinicstart.controllers;
 
+import com.projectx.petclinicstart.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping({"", "/index"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
