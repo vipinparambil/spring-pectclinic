618c3d0387f86620919cbda69342be0da4619f43
vipinparambil <vipinparambil@gmail.com>
2020-05-10 19:02:04 +0530

    Created Base Entity

--- pet-clinic-start/pet-clinic-data/src/main/java/com/projectx/petclinicstart/model/BaseEntity.java
+++ pet-clinic-start/pet-clinic-data/src/main/java/com/projectx/petclinicstart/model/BaseEntity.java
@@ -1,24 +1,30 @@
 package com.projectx.petclinicstart.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-public class BaseEntity implements Serializable{
+@MappedSuperclass
+public class BaseEntity implements Serializable {
 
     /**
      *
      */
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
-        return this.id;
+        return id;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
-
 
 
 }
